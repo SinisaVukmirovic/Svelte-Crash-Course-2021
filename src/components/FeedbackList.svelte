01e164de29f7896bfee1660cf79eb9df58953536
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2021-11-30 18:52:28 +0100

    Added transition anims to list items

--- src/components/FeedbackList.svelte
+++ src/components/FeedbackList.svelte
@@ -1,9 +1,14 @@
 <script>
+    import { fade, scale } from 'svelte/transition';
     import FeedbackItem from './FeedbackItem.svelte';
     export let feedback = [];
 </script>
 
 {#each feedback as fb (fb.id)}
                     <!-- forwarding dispatch event to the above compoents -->
-    <FeedbackItem item={fb} on:delete-feedback />
+    <!-- transitions can't be added on a custom component, -->
+    <!-- so, we wrap it in a div  -->
+    <div in:scale out:fade>
+        <FeedbackItem item={fb} on:delete-feedback />
+    </div>
 {/each}
